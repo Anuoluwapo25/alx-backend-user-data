dfa21e789fc65370e0f193781fd261aefacd619e
Anuoluwapo25 <anuolwapoali25@gmail.com>
2024-07-05 00:39:00 +0000

    logging

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,21 +1,19 @@
-#!/usr/bin/env python3
-"""
-function that return lod
-g mess
-"""
+import logging
 
 
-import re
-from typing import List
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
 
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
 
-def filter_datum(fields: List[str],
-                 redaction: str, message: str, separator: str) -> str:
-    """
-    return the log message obfuscated
-    """
-    for field in fields:
-        msg_obfuscated = re.sub(field + "=.*?" + separator,
-                                field + "=" + redaction + separator,
-                                message)
-    return msg_obfuscated
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        message = record.getMessage()
+        for field in self.fields:
+            message = re.sub(f'{field}=[^;]*', f'{field}={self.REDACTION}', message)
+        return super().format(record)
